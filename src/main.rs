d9af224157ed0b1efb717f8382d86d25cf303660
Robert Flitney <robert.ogflitney@gmail.com>
2022-06-08 19:04:14 +0100

    redesigned to reduce memory usage

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,145 @@
-use std::env;
+use std::{env, fmt::Display};
 
-#[derive(Clone)]
 struct Solution {
   target: usize,
-  possible: bool,
-  necessary_coins: Option<Vec<usize>>
+  coins_used: usize,
+  next_coin: Option<usize>
 }
 
 impl Solution {
-  fn to_string(solution: &Solution) -> String {
-    format!("{}: {}", solution.target,
-      match &solution.necessary_coins {
-        None => format!("impossible"),
-        Some(coins) => match coins.len() {
-          0 => format!("no change needed"),
-          _ => format!("{}", coins.iter().map(|s| format!("{s}")).collect::<Vec<String>>().join(" "))
-        }
-      })
+  fn new(target: usize) -> Self {
+    Self {
+      target: target,
+      coins_used: 0,
+      next_coin: None
+    }
+  }
+
+  fn is_possible(&self) -> bool {
+    // if there is a next coin, or no change is needed, there is a solution
+    self.target == 0 || self.next_coin.is_some()
+  }
+
+  fn set_next_coin(&mut self, coin: usize, coins_used: usize) {
+    self.next_coin = Some(coin);
+    self.coins_used = coins_used;
   }
 }
 
-trait Countable<T> {
-  fn count_where<P>(self: &Self, predicate: P) -> usize where P: Fn(&T) -> bool;
-}
-
-impl Countable<usize> for &[usize] {
-  fn count_where<P>(&self, predicate: P) -> usize where
-  P: Fn(&usize) -> bool {
-    self.iter().fold(0, |acc, i| {
-      match predicate(i) {
-        true => acc + 1,
-        false => acc
-      }
-    })
-  }
-}
-
-impl Countable<usize> for &Vec<usize> {
-  fn count_where<P>(&self, predicate: P) -> usize where
-  P: Fn(&usize) -> bool {
-    self.iter().fold(0, |acc, i| {
-      match predicate(i) {
-        true => acc + 1,
-        false => acc
-      }
-    })
-  }
-}
-
-fn generate_partials(target: usize) -> Vec<Solution>{
-  let mut ret = Vec::new();
-
-  ret.push(Solution {
-    target: 0,
-    possible: true,
-    necessary_coins: Some(vec![]),
-  });
-
-  for i in 1..=target {
-    ret.push(Solution {
-      target: i,
-      possible: false,
-      necessary_coins: None 
-    })
-  }
-
-  ret
-}
-
-fn available_coin<C1, C2>(coin: usize, from_coins: C1, without_coins: C2) -> bool where
-  C1: Countable<usize>,
-  C2: Countable<usize> {
-  from_coins.count_where(|c| *c == coin) -
-  without_coins.count_where(|c| *c == coin)
-  > 0
-}
-
-fn all_solutions(target: usize, coins: &Vec<usize>) -> Vec<Solution> {
-  let mut partials = generate_partials(target);
-
+fn solutions(coins: Vec<usize>) -> Vec<Solution> {
+  // sort coins, largets to smallest
+  // optimisation for later
   let mut coins = coins.clone();
   coins.sort();
   coins.reverse();
 
-  for target in 1..=target {
-    let mut partial = partials[target].clone();
+  // calculate max target (sum of coin values)
+  let max_target: usize = coins.iter()
+    .fold(0, |a, b| a + b);
 
-    for coin in coins.iter() {
-      if target < *coin { continue }
+  // build mutable solutions for each target
+  let mut solutions: Vec<Solution> = (0..=max_target)
+    .map(|t| Solution::new(t)).collect();
 
-      let next_inter = partials[target - coin].clone();
-      if !next_inter.possible { continue }
+  // for each coin from largest to smallest
+  for coin in coins.iter() {
+    // for each target from largest to smallest
+    for target in (0..=max_target).rev() {
+      // subgoal <- target - coin
+      let subgoal = target.checked_sub(*coin);
+      if subgoal.is_none() { continue }
+      let subgoal = subgoal.unwrap();
 
-      let next_coins = next_inter.necessary_coins.unwrap();
+      let subgoal_solution: Option<&Solution> = solutions.get(subgoal);
 
-      if available_coin(*coin, &coins, &next_coins) {
-        let mut new_coins = next_coins;
-        new_coins.push(*coin);
+      // if subgoal is possible then goal is possible with this coin
+      let subgoal_is_possible: bool = subgoal_solution
+        .map(|s| s.is_possible())
+        .unwrap_or(false);
 
-        partial.possible = true;
-        partial.necessary_coins = Some(new_coins);
-        break;
+      if subgoal_is_possible {
+        let solution = &solutions[target];
+
+        // if new coin will reduce the number of coins, update the next coin
+        let subgoal = subgoal_solution.unwrap();
+        let new_coins_used = subgoal.coins_used + 1;
+        if solution.is_possible() && new_coins_used >= solution.coins_used { continue }
+
+        solutions[target].set_next_coin(*coin, new_coins_used);
       }
+      // otherwise try the next coin
     }
-
-    partials[target] = partial.clone();
   }
 
-  // this is not necessary for the program to function, it just looks nicer
-  for i in 0..=target {
-    let mut partial = partials[i].clone();
+  // return solutions
+  solutions
+}
 
-    partial.necessary_coins = partial.necessary_coins.and_then(|cs| {
-      let mut cs = cs;
-      cs.sort();
-      cs.reverse();
-      Some(cs)
-    });
+fn coins_for(target: usize, solutions: &Vec<Solution>) -> Option<Vec<usize>> {
+  let mut target = target;
+  let solution = solutions.get(target)?;
 
-    partials[i] = partial;
+  if !solution.is_possible() { return None }
+
+  let mut ret = vec![];
+
+  let mut next_coin = solution.next_coin;
+
+  while let Some(coin) = next_coin {
+    target -= coin;
+    ret.push(coin);
+    let solution = solutions.get(target);
+    if let Some(solution) = solution {
+      next_coin = solution.next_coin
+    } else { next_coin = None }
   }
 
-  partials
+  Some(ret)
 }
 
-#[allow(dead_code)]
-fn find_solution(target: usize, coins: &Vec<usize>) -> Solution {
-  all_solutions(target, coins).last().unwrap().clone()
+trait StringJoinable<T> {
+  fn join_with(&mut self, joiner: &str) -> String;
 }
 
+impl<I, T> StringJoinable<T> for I where I: Iterator<Item = T>, T: Display {
+    fn join_with(&mut self, joiner: &str) -> String {
+        let mut ret = "".to_string();
+        let mut next = self.next();
+        while let Some(value) = next {
+          ret.push_str(&*value.to_string());
+          next = self.next();
+          if next.is_some() {
+            ret.push_str(&*joiner.to_string());
+          }
+        }
+        ret
+    }
+}
 
 fn main() {
+
+  // read coins from command
   let coins: Vec<usize> = env::args()
     .map(|s| s.parse())
-    .filter(|u| u.is_ok())
-    .map(|u| u.unwrap())
-    .collect();
+    .flatten().collect();
+  
+  // calculate solutions
+  let solutions: Vec<Solution> = solutions(coins);
 
-  let target: usize = coins.iter().fold(0, |acc, i| acc + i);
+  // print solutions
+  for target in 0..solutions.len() {
+    let solution = solutions.get(target).unwrap();
+    // if !solution.is_possible() { continue }
 
-  let solutions = all_solutions(target, &coins);
-  let solutions: Vec<String> = solutions.iter().map(Solution::to_string).collect();
+    print!("{}: ", solution.target);
+    
+    if let Some(coins) = coins_for(target, &solutions) {
+      if coins.len() == 0 { print!("no change needed") }
+      print!("{}", coins.iter().join_with(", "))
+    } else {
+      print!("not possible")
+    }
 
-  println!("{}", solutions.join("\n"));
+    println!();
+  }
 }
